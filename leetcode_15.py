bd8601407afd0c5ab90e53f8253bbb453f14a519
Enes Görkem Genç <gencenesgorkem@outlook.com>
2024-06-28 23:46:18 +0100

    Day 4

--- leetcode_15.py
+++ leetcode_15.py
@@ -23,3 +23,47 @@ def three_sum_attempt_1(nums): #28m
                 triplets_map[''.join(str(x) for x in sorted([number_1, number_2, number_to_look_for]))] = [number_1, number_2, number_to_look_for]
             
     return triplets_map.values()
+
+
+#Attempt 2
+def three_sum(nums):
+    
+    triplets = []
+
+    number_index_map = {}
+
+    nums.sort()
+    #[-4, -1, -1, 0, 1, 2]
+
+    for i in range(len(nums)):
+        number_index_map[nums[i]] = i
+
+    lp, rp = 0, len(nums) - 1
+    
+    while lp < rp:
+        number_left = nums[lp]
+        number_right = nums[rp]
+
+        if rp == lp + 1:
+            rp = len(nums) - 1
+            lp += 1
+            continue
+
+        if number_left == nums[lp - 1]:
+            lp += 1
+            continue
+
+        if rp < len(nums) - 2 and number_right == nums[rp + 1]:
+            rp -= 1
+            continue
+
+        number_to_look_for = -1 * (number_left + number_right)
+
+        if number_to_look_for in number_index_map and (number_index_map[number_to_look_for] != lp and number_index_map[number_to_look_for] != rp):
+            triplets.append([number_left, number_right, number_to_look_for])
+
+        rp -= 1
+
+    return triplets
+
+print(three_sum([-1,0,1,2,-1,-4]))
